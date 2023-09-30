8d8684d7467ca57700f6ba31122cd805fcd6e7a5
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-30 13:44:05 -0700

    Add fix for linux, adjust remote session

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-qmake                       \
-  QMAKE_CC=$CC_FOR_BUILD    \
-  QMAKE_CXX=$CXX_FOR_BUILD  \
-  QMAKE_LINK=$CXX_FOR_BUILD \
+qmake                                \
+  QMAKE_CXXFLAGS="-I$PREFIX/include" \
+  QMAKE_CC=$CC_FOR_BUILD             \
+  QMAKE_CXX=$CXX_FOR_BUILD           \
+  QMAKE_LINK=$CXX_FOR_BUILD          \
   polyview.pro
 make -j${CPU_COUNT}
 make install INSTALL_ROOT=$PREFIX
