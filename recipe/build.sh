88b6f5dd089c949f4b1de2e368a94cc659892f94
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2023-09-30 11:54:20 -0700

    Turn off Windows build, tune the Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-qmake polyview.pro
+qmake                       \
+  QMAKE_CC=$CC_FOR_BUILD    \
+  QMAKE_CXX=$CXX_FOR_BUILD  \
+  QMAKE_LINK=$CXX_FOR_BUILD \
+  polyview.pro
 make -j${CPU_COUNT}
 make install INSTALL_ROOT=$PREFIX
